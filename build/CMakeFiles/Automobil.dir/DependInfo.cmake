8772662b38ef0dcb5ec6b4eca5ad569f262eb5a5
RazvanDraghia <126500562+RazvanDraghia@users.noreply.github.com>
2024-11-03 17:35:05 +0200

    Add files via upload
    Fisierul Build

---
+++ build/CMakeFiles/Automobil.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/razvan/Desktop/TemaPAOO/src/Automobil.cpp" "/home/razvan/Desktop/TemaPAOO/build/CMakeFiles/Automobil.dir/src/Automobil.cpp.o"
+  "/home/razvan/Desktop/TemaPAOO/src/main.cpp" "/home/razvan/Desktop/TemaPAOO/build/CMakeFiles/Automobil.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Automobil.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Automobil"
+  "Automobil.pdb"
+  "CMakeFiles/Automobil.dir/src/Automobil.cpp.o"
+  "CMakeFiles/Automobil.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Automobil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
