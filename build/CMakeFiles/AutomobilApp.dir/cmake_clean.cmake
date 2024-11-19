db63ac4c02f24671efbe29009d689d1594339ebf
RazvanDraghia <126500562+RazvanDraghia@users.noreply.github.com>
2024-11-19 21:32:13 +0200

    Add files via upload
    noul fisier Build

---
+++ build/CMakeFiles/AutomobilApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AutomobilApp"
+  "AutomobilApp.pdb"
+  "CMakeFiles/AutomobilApp.dir/src/automobil.cpp.o"
+  "CMakeFiles/AutomobilApp.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AutomobilApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
